0295a5aa6a5b015792f8d3925280ca4762a28002
兰伟 <guai520a@gmail.com>
2020-09-10 13:59:46 +0800

    使用PostgreSQL

--- config/database.php
+++ config/database.php
@@ -1,5 +1,5 @@
 <?php
-
+$url = parse_url(getenv("DATABASE_URL"));
 return [
 
     /*
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,11 +56,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', $url["host"]),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', substr($url["path"], 1)),
+            'username' => env('DB_USERNAME', $url["user"]),
+            'password' => env('DB_PASSWORD', $url["pass"]),
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
